50dcd5ce50fc2b5eadf5de32224108dde9d8d5a1
kishan vasani <70906939+mrvasani48@users.noreply.github.com>
2021-04-17 01:38:58 +0530

    Add files via upload

---
+++ home_page.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gtu Quick Info</title>
+    <link rel="stylesheet" href="home_page.css">
+    <script src="home_page.js"></script>
+</head>
+<body> 
+    <header>  
+            <img src="image/logo.jpg" height="150px" width="150px"></img>  
+            <h1>gujrat technological University gandhinagar</h1>
+            <Nav>
+                  <a href="#">Home</a>
+                  <a href="#">About Gtu</a>
+                  <a href="#">Circular</a>
+                  <a href="#">Scadamic</a>
+                  <a href="#">Stu. Zone</a>
+                  <a href="#">Result</a>
+                  <a href="#">Cources</a>
+                  <a href="#">Other</a>
+                  <a href="#">Contact Us</a>
+                  <a href="#">login_page</a>
+            </Nav>
+    </header>
+            <hr>
+         
+    <section>
+            <img src="https://www.gtu.ac.in/SliderImages/images/Final_Slider_Women'sDayCelebration2M8A8918.jpg" height="280px" width="280px"></img> 
+            <img src="https://www.gtu.ac.in/SliderImages/images/Final_Slider_Women'sDayCelebration2M8A8918.jpg" height="280px" width="280px"></img> 
+            <img src="https://www.gtu.ac.in/SliderImages/images/Final_Slider_Women'sDayCelebration2M8A8918.jpg" height="280px" width="280px"></img> 
+            <img src="https://www.gtu.ac.in/SliderImages/images/Final_Slider_Women'sDayCelebration2M8A8918.jpg" height="280px" width="280px"></img> 
+
+            <ul style="list-style-type:none;">
+            <li><a href="#"><</a></li>
+            <li><a href="#">*</a></li>
+            <li><a href="#">*</a></li>
+            <li><a href="#">*</a></li>
+            <li><a href="#">*</a></li>
+            <li><a href="#">></a></li>
+            </ul>
+
+    </section>
+<hr>
+    <section>
+            <Nav>
+                  <a href="#">Reports</a>
+                  <a href="#">Gallery</a>
+                  <a href="#">Impo. Link</a>
+                  <a href="#">Admission</a>
+                  <a href="#">International</a>
+                  <a href="#">E-Education</a>
+                  <a href="#">Online Payment</a>
+                  <a href="#">Download</a>
+        
+            </Nav>
+    </section>
+    <hr>
+    <section>
+              <video controls autoplay="none" height="380" width="660" alt="this video please check ypur connection" >
+              <source type="video/mp4" src="https://www.youtube.com/embed/ur7enXZE8As">
+              </source>
+              </video>
+              <h3>
+              <span>welcome to </span>
+              Gujrat technological University of  gandhinagar
+              </h3>
+              <p>
+              India's internationally renowned The Maharaja Sayajirao University of Baroda (1949)<br>
+               has blossomed from the Baroda College (1881) which is one of the oldest centres of <br>
+               learning in western India. MSU is the only state University with a residential, <br>
+                unitary character having English as sole medium of instruction with more than 1200  <br>
+                well-qualified faculty members and 1500 administrative staff to facilitate the  <br>
+                learning of more than 35000 students having cosmopolitan life on campus. <br>
+              </p>
+          
+    </section>
+    <hr>
+    <section>
+                <div>
+                     <img src="image/all.jpg" height="280px" width="280px"></img>                 
+                     <img src="image/all.jpg" height="280px" width="280px"></img>                  
+                     <img src="image/all.jpg" height="280px" width="280px"></img> 
+                     <img src="image/all.jpg" height="280px" width="280px"></img>
+                     <h1>faculty of engineering</h1>
+                     <h1>faculty of engineering</h1>
+                     <h1>faculty of engineering</h1>
+                     <h1>faculty of engineering</h1> 
+               </div>
+               <div>
+                     <img src="image/all.jpg" height="280px" width="280px"></img>                 
+                     <img src="image/all.jpg" height="280px" width="280px"></img>                  
+                     <img src="image/all.jpg" height="280px" width="280px"></img> 
+                     <img src="image/all.jpg" height="280px" width="280px"></img>
+                     <h1>faculty of engineering</h1>
+                     <h1>faculty of engineering</h1>
+                     <h1>faculty of engineering</h1>
+                     <h1>faculty of engineering</h1> 
+               </div>
+               <div>
+                     <img src="image/all.jpg" height="280px" width="280px"></img>                 
+                     <img src="image/all.jpg" height="280px" width="280px"></img>                  
+                     <img src="image/all.jpg" height="280px" width="280px"></img> 
+                     <img src="image/all.jpg" height="280px" width="280px"></img>
+                     <h1>faculty of engineering</h1>
+                     <h1>faculty of engineering</h1>
+                     <h1>faculty of engineering</h1>
+                     <h1>faculty of engineering</h1> 
+               </div>
+               <div>
+                     <img src="image/all.jpg" height="280px" width="280px"></img>                 
+                     <img src="image/all.jpg" height="280px" width="280px"></img>                  
+                     <img src="image/all.jpg" height="280px" width="280px"></img> 
+                     <img src="image/all.jpg" height="280px" width="280px"></img>
+                     <h1>faculty of engineering</h1>
+                     <h1>faculty of engineering</h1>
+                     <h1>faculty of engineering</h1>
+                     <h1>faculty of engineering</h1> 
+               </div>
+    </section>
+<hr>
+<br>
+    <section>
+    <h1 align="center">Event</h1>
+             <a   class="Courrse"    href="#"  target=""  rel="noopener noreferrer"  >
+             <img height="300px" width="250px"  src="image/all.jpg" alt="this is image " />
+             </a>
+    </section>
+    <hr>
+   <div>
+    <section>
+    <h1 align="center">News </h1>
+    <a style="float:left"  href="#"><</a>
+             <a style="float:left"  class="Courrse"    href="#"  target=""  rel="noopener noreferrer"  >
+            revised Online Midsemester<br> Exam Schedule SSBCOM <br>III Apr.....
+             </a>
+             <a style="float:left"   class="Courrse"    href="#"  target=""  rel="noopener noreferrer"  >
+            revised Online Midsemester<br> Exam Schedule SSBCOM <br>III Apr.....
+             </a>
+             <a style="float:left"   class="Courrse"    href="#"  target=""  rel="noopener noreferrer"  >
+            revised Online Midsemester<br> Exam Schedule SSBCOM <br>III Apr.....
+             </a>
+             <a  style="float:left"  class="Courrse"    href="#"  target=""  rel="noopener noreferrer"  >
+            revised Online Midsemester<br> Exam Schedule SSBCOM <br>III Apr.....
+             </a>
+             <a style="float:left"    class="Courrse"    href="#"  target=""  rel="noopener noreferrer"  >
+            revised Online Midsemester<br> Exam Schedule SSBCOM <br>III Apr.....
+             </a>
+             <a  style="float:left"  class="Courrse"    href="#"  target=""  rel="noopener noreferrer"  >
+            revised Online Midsemester<br> Exam Schedule SSBCOM <br>III Apr.....
+             </a>
+             <a href="#">></a>
+    </section>
+    
+ </div>
+
+   <br>
+    <section>
+    <h1 align="center">circular</h1>
+             <a   class="Course"    href="#"  target=""  rel="noopener noreferrer"  >
+             <img height="300px" width="250px"  src="image/all.jpg" alt="this is image " />
+             </a>
+    </section>
+    <hr>
+    <footer>
+            <h1>contact us</h1>
+              <img src="image/logo.jpg" alt="gtu" ></img>
+              <h4>Gujrat Technological University <br> gandhinagar</h4>
+              <p>Address<br>
+Gujarat Technological University<br>
+Nr.Vishwakarma Government Engineering College Nr.Visat Three Roads, Visat - Gandhinagar Highway<br>
+Chandkheda, Ahmedabad – 382424 - Gujrat</p>
+<span style="font-size: 15px; color: #FF6300;">&nbsp;&nbsp;079-23267521/570</span>
+<a href="mailto:registrar@gtu.ac.in">registrar@gtu.ac.in</a>
+    </footer>
+    <hr>
+    <nav>
+    <p>Copyright © 2019  Gujrat Technological University .   All Rights Reserved.</p>
+    <a href="#" target="_blank" rel="noopener noreferrer">Term & condition</a>
+    <a href="#" target="_blank" rel="noopener noreferrer">gtu</a>
+    </nav>
+    <hr>
+</body>
+</html>
