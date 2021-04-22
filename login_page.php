4046e5ed9cd6bfd2398643e2c35a3d6820c59404
Hardik Santoki <65931558+hardiksantoki@users.noreply.github.com>
2021-04-22 14:10:50 +0530

    login page php file
    final changes from my side.

--- login_page.php
+++ login_page.php
@@ -1,34 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>sign-in</title>
-    <link rel="stylesheet" href="login_page.css" />
-   <script type="text/javascript" src="login_validation.js"></script>
-  </head>
-  <body>
-        <div id="sign-in">
-          <h1 id="head">sign-in</h1>
-          <form  action="login_page_validation.php" method="GET">
-               <label for="user">user id : </label>
-               
-               <input onchange="validateuserid()"type="text" name="user_id" id="user_id"  placeholder="Enrollment no"><br>  <br>
 
-               <label for="password">password : </label>
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>sign-in</title>
+  <link rel="stylesheet" href="login_page.css" />
+  <script type="text/javascript" src="login_validation.js"></script>
+</head>
 
-                <input type="password" name="password"  placeholder="password" onchange="validatepassword()" id="password"><br>
-                <span><?php if(isset($_GET['error'])){echo $_GET['error'];}?></span>
-              <p id="first"><a href="#" class="first1" > forgot username / password ?</a> </p>
+<body>
+  <div id="a">
+    <div id="sign-in">
+      <div id="head">
+        <h2 align="center">GTU QUICK INFO</h2>
+      </div>
 
-              <div align="center"> <input  type="submit"  value="sign in" ></div> 
+      <dic class="container">
+        <form class="form1" onsubmit="return validatepassword(),validateuserid()">
+          <div class="i"><label for="user">user id : </label>&nbsp;
 
-              <div id="addac"> don't have an account ?</div>
-            </form> 
-              <div id="second" >  <a class="first1"   href="sign_uppage.html"  >sign up now</a> </div>
-         
+            <input onchange="validateuserid()" type="text" name="user_id" id="user_id" placeholder="Enrollment no">
+          </div><br>
+          <div class="i">
+            <label for="password">password : </label>&nbsp;
+
+            <input type="password" name="password" placeholder="password" onchange="validatepassword()" id="password">
+            
+          </div>
+          <span><?php if(isset($_GET['error'])){echo $_GET['error'];}?></span>
+
+          <p id="first"><a href="#" class="first1"> forgot username / password ?</a> </p><br>
+
+          <div align="center"> <input class="first1" type="submit" value="sign in"></div><br>
+
+          
+        </form>
+        <div id="addac"><span>don't have an account ?
+        <a class="first1" href="sign_uppage.html">sign up now</a></span>
         </div>
-      
-  </body>
+      </dic>
+
+
+
+    </div>
+  </div>
+</body>
+
 </html>
